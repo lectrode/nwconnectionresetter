d1cb300d698696d511d94a6876b41af6b81ce847
electrodexs@gmail.com <electrodexs@gmail.com@2bca2f4c-de36-8297-f5b0-aa85221f1791>
2012-08-03 06:50:18 +0000

    BUGFIX: verify downloaded versions file when path has spaces

--- Network_Resetter.bat
+++ Network_Resetter.bat
@@ -7,7 +7,7 @@ CALL :INITPROG
 REM -----Program Info-----
 REM Name: 		Network Resetter
 REM Revision:
-	SET rvsn=r168
+	SET rvsn=r169
 REM Branch:
 	SET Branch=
 
@@ -1526,7 +1526,7 @@ GOTO :EOF
 SET SU_Cur_Valid=1
 IF NOT EXIST "%*" SET SU_Cur_Valid=0&GOTO :EOF
 SET SU_Cur_numlines=0
-FOR /F "tokens=* DELIMS=" %%c IN (%*) DO SET /A SU_Cur_numlines+=1
+FOR /F "usebackq tokens=* DELIMS=" %%c IN ("%*") DO SET /A SU_Cur_numlines+=1
 IF %SU_Cur_numlines% LEQ 3 SET SU_Cur_Valid=0&DEL /F /S /Q "%*"
 GOTO :EOF
 
